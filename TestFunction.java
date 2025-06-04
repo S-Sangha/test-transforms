146bafee91eafc8bd9d1c4cd09857c6c0f110098
S-Sangha <shannon@etleap.com>
2025-06-04 14:26:11 -0700

    add valid custom transform

---
+++ TestFunction.java
@@ -0,0 +1,8 @@
+import java.util.Collections;
+import java.util.Map;
+
+public class TestFunction {
+    public Map<String, String> apply(String input1, String input2) {
+        return Collections.singletonMap("foo", "bar");
+    }
+}
